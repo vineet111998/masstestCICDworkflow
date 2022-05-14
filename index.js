dff57b0f3a8467af09dad05177c71660e0d0dfb1
gitVineet <rjvkrs11@gmail.com>
2022-05-14 16:54:55 +0530

    Vineet first commit
    Signed-off-by: gitVineet <rjvkrs11@gmail.com>

---
+++ index.js
@@ -0,0 +1,29 @@
+const express =require('express');
+const port ='8000';
+const app =express();
+
+const cors=require('cors');
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+
+  app.get('/',(req,res)=>{
+    res.send("In hello World!!");
+});
+
+app.get('/getData',(req,res)=>{
+    res.send("In Get Data!!");
+});
+
+app.listen(port, ()=>{
+    console.log("Listening to port: "+ port)
+});
